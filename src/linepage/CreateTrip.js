48c23ea734519ea03b1171e66c87f769bbe50174
romncn <romncn@gmail.com>
2020-04-02 23:58:53 +0700

    [UPDATE]CreateTrip.js -content-page

--- src/linepage/CreateTrip.js
+++ src/linepage/CreateTrip.js
@@ -5,6 +5,17 @@ import NavTripPage from '../components/Nav/NavTripPage';
 import FooterTripPage from '../components/Footer/FooterTripPage';
 
 
+const InputFrom = (props) => {
+    return (
+        <div className="InputFrom">
+            <div class="col pt-4">
+                <label for="exampleInputEmail1">{props.inputname}</label>
+                <input type={props.inputtype} class="form-control" id={props.inputid} placeholder={props.textinplaceholder} />
+            </div>
+        </div>
+    )
+}
+
 class CreateTrip extends React.Component {
     render() {
         return (
@@ -14,7 +25,15 @@ class CreateTrip extends React.Component {
                     <NavTripPage firsttitle={"ยินดีต้อนรับ"} secondtitle={"เริ่มสร้างแผนการท่องเที่ยว"} ></NavTripPage>
                 </div>
                 <div className="content-page">
-                    Natchanon
+                    <form>
+                        <div class="form-group">
+                            <InputFrom inputname="ชื่อทริป" inputtype="email" inputid="exampleInputEmail1" textinplaceholder="ใส่ชื่อทริปท่องเที่ยว"></InputFrom>
+                            <InputFrom inputname="จังหวัด" inputtype="email" inputid="exampleInputEmail1" textinplaceholder="ใส่ชื่อจังหวัดที่ต้องการเดินทาง"></InputFrom>
+                            <InputFrom inputname="วันที่เริ่มเดินทาง" inputtype="date" inputid="example-date-input" textinplaceholder="mm-dd-yyyy"></InputFrom>
+            
+                        </div>
+
+                    </form>
                 </div>
                 <div className="buttom-page">
                     <Link to="/CreateTripPerDay" >
