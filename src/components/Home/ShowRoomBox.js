42b1d6118e1d0ea003a2fc863cdddb359a844709
romncn <romncn@gmail.com>
2020-04-10 16:11:05 +0700

    [UPDATE]start used sweetalert2

--- src/components/Home/ShowRoomBox.js
+++ src/components/Home/ShowRoomBox.js
@@ -2,47 +2,63 @@ import React from 'react';
 import "../../static/css/Show-Room.css";
 import BgSlide1 from '../../static/img/bg-slide-test-1.jpg';
 import Logo from '../../static/img/logojourney.png';
+import Swal from 'sweetalert2';
 
 
-const ShowRoomBox = () => {
-    return (
-        <section className="py-5">
-            <div className="Show-Join-Room">
-                <div className="container">
-                    <div className="row">
 
-                        <div className="col-4">
-                            <div className="col">
-                            <img src={Logo} height="45" alt="MrJourney" />
-                                <div class="alert box-room show-box">
-                                    <img class="d-block w-100" src={BgSlide1} alt="First slide" />
-                                    <div class="box-room-details show-box mt-2">
-                                        <div className="mt-3 mr-3 ml-3 mb-0">
-                                            <h3 className="py-1">Let'Go Chonburi !</h3>
-                                            <span className="py-1">Province : </span>
-                                            <br /><span className="py-1">Date : </span>
-                                            <p/><i class="fas fa-user fa-lg ml-2" style={{color:"dodgerblue"}}></i>
-                                            <i class="fas fa-user fa-lg ml-2 mb-0" style={{color:"hotpink"}}></i>
-                                            <br/><span className="mt-0 ml-2" style={{fontSize:"8px"}}>อายุ 20 ปีขึ้นไป</span>
-                                            <p className="text-right" style={{fontSize:"13px"}}>ผู้สร้าง chutikann</p>
-                                        </div>
-                                        <div className="navbar">
-                                            <div>
-                                                <button type="button" class="btn btn-warning text-white">Join</button>
+class ShowRoomBox extends React.Component {
+    AlertJoinRoom = () => {
+        Swal.fire({
+            icon: 'error',
+            title: 'ขออภัย!',
+            text: 'เงื่อนไขไม่ตรงกับทางทริปที่กำหนด',           
+            showCancelButton: false,
+            confirmButtonColor: '#D33',
+            confirmButtonText: 'กลับสู่หน้าหลัก'
+        })
+    }
+    render() {
+        return (
+            <section className="py-5">
+                <div className="Show-Join-Room">
+                    <div className="container">
+                        <div className="row">
+
+                            <div className="col-4">
+                                <div className="col">
+                                    <img src={Logo} height="45" alt="MrJourney" />
+                                    <div class="alert box-room show-box">
+                                        <img class="d-block w-100" src={BgSlide1} alt="First slide" />
+                                        <div class="box-room-details show-box mt-2">
+                                            <div className="mt-3 mr-3 ml-3 mb-0">
+                                                <h3 className="py-1">Let'Go Chonburi !</h3>
+                                                <span className="py-1">Province : </span>
+                                                <br /><span className="py-1">Date : </span>
+                                                <p /><i class="fas fa-user fa-lg ml-2" style={{ color: "dodgerblue" }}></i>
+                                                <i class="fas fa-user fa-lg ml-2 mb-0" style={{ color: "hotpink" }}></i>
+                                                <br /><span className="mt-0 ml-2" style={{ fontSize: "8px" }}>อายุ 20 ปีขึ้นไป</span>
+                                                <p className="text-right" style={{ fontSize: "13px" }}>ผู้สร้าง chutikann</p>
                                             </div>
-                                            <div className="button-search">
-                                            <i class="fas fa-search text-warning fa-2x"></i>
+                                            <div className="navbar">
+                                                <div>
+                                                    <button type="button" class="btn btn-warning text-white" onClick={this.AlertJoinRoom}>Join</button>
+                                                </div>
+                                                <div className="button-search">
+                                                    <i class="fas fa-search text-warning fa-2x"></i>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
+                            
+
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 }
 export default ShowRoomBox;
--- src/components/Nav/NavWebPage.js
+++ src/components/Nav/NavWebPage.js
@@ -1,56 +1,71 @@
 import React from 'react';
 import Logo from '../../static/img/logojourney.png';
+import Swal from 'sweetalert2';
 
 
-const NavWebPage = () => {
+// const NavWebPage = () => {
+class NavWebPage extends React.Component {
+    Alert = () => {
+        Swal.fire({
+            title: 'อยากสร้างห้องงั้นหรอ?',
+            text: 'Login ก่อนสิ!',           
+            showCancelButton: false,
+            confirmButtonColor: '#F37945',
+            confirmButtonText: 'Login'
+        })
+    }
 
-    return (
+    render() {
+        return (
 
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-danger" style={{ color: "white" }}>
-                <a href="/Home" class="navbar-brand">
-                    <img src={Logo} height="45" alt="MrJourney" />
-                </a>
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav ml-auto">
-                        <li className="nav-item mr-2">
-                            <form class="form-inline">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
-                            </form>
-                        </li>
-                        <li className="nav-item">
-                            <button type="button" class="btn btn-warning mb-2 mr-2 text-white">Create Room
+
+
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-danger" style={{ color: "white" }}>
+                    <a href="/Home" class="navbar-brand">
+                        <img src={Logo} height="45" alt="MrJourney" />
+                    </a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav ml-auto">
+                            <li className="nav-item mr-2">
+                                <form class="form-inline">
+                                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                                    <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+                                </form>
+                            </li>
+                            <li className="nav-item">
+                                <button type="button" class="btn btn-warning mb-2 mr-2 text-white" onClick={this.Alert}>Create Room
                                 <i className="fas fa-plus pl-1" style={{ color: "white" }}></i>
-                            </button>
-                        </li>
+                                </button>
+                            </li>
 
-                        <li class="nav-item dropdown ">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-user-circle fa-lg text-white"></i> <span className="text-white">Guest</span> </a>
-                            <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
-                                <a class="dropdown-item " href="#">My account</a>
-                                <a class="dropdown-item " href="#">Joined Room</a>
-                                <a class="dropdown-item " href="#">My Owner Room</a>
-                                <a class="dropdown-item " href="#">Log out</a>
-                            </div>
-                        </li>
-
-
-                    </ul>
-                </div>
-            </nav>
-        </div>
+                            <li class="nav-item dropdown ">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-user-circle fa-lg text-white"></i> <span className="text-white">Guest</span> </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+                                    <a class="dropdown-item " href="#">My account</a>
+                                    <a class="dropdown-item " href="#">Joined Room</a>
+                                    <a class="dropdown-item " href="#">My Owner Room</a>
+                                    <a class="dropdown-item " href="#">Log out</a>
+                                </div>
+                            </li>
 
 
+                        </ul>
+                    </div>
+                </nav>
+            </div>
 
 
 
 
-    )
+
+
+        )
+    }
 }
 export default NavWebPage;
